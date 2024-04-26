e6c23750a7a5060ae63b2034ace8bdeffb05151d
Balazs Kegl <balazs.kegl@gmail.com>
2024-04-27 00:49:19 +0800

    [fix] xgboost elif

--- ramphy/ramp_setup/workflow_elements/tabular_regressors/xgboost.py
+++ ramphy/ramp_setup/workflow_elements/tabular_regressors/xgboost.py
@@ -34,7 +34,7 @@ class Regressor(BaseEstimator):
         score_name = metadata["score_name"]
         if score_name in ["mse", "rmse"]:
             self.objective = "reg:squarederror"
-        if score_name in ["rmsle"]:
+        elif score_name in ["rmsle"]:
             self.objective = "reg:squaredlogerror"
         elif score_name == "mae":
             self.objective = "reg:absoluteerror"
