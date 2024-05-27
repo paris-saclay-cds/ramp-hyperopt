cde6fc17b36f8b62d28fd765d3cbbc67eb688431
Giuseppe Paolo <giuseppe.paolo93@gmail.com>
2024-05-27 23:00:27 +0800

    [feat] comment pangu setup action

--- ramphy/ramp_setup/pangu_actions.py
+++ ramphy/ramp_setup/pangu_actions.py
@@ -11,6 +11,17 @@ from ramphy import ramp_setup as rs
 def pangu_setup_kit(
     pangu_root: str | Path, output_path: str | Path, challenge_name: str, llm: str = "fschat/llama-3-70B-Instruct"
 ) -> Dict:
+    """Runs the pangu auto setup
+
+    Args:
+        pangu_root (str | Path): Path to pangu installation
+        output_path (str | Path): Path to where to save the setup
+        challenge_name (str): Name of the kaggle challenge
+        llm (str, optional): LLM config. Defaults to "fschat/llama-3-70B-Instruct".
+
+    Returns:
+        Dict: Status of the setup
+    """
     env_args = {"HYDRA_FULL_ERROR": 1, "PANGU_DEBUG": 1}
     script_path = Path(pangu_root) / "src" / "pangu" / "start.py"
     hydra_args = {
