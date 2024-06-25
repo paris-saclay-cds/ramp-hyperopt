95cfb82bc01f9ef404ccceba67effe92fb29a150
Giuseppe Paolo <giuseppe.paolo93@gmail.com>
2024-06-25 15:41:37 +0000

    [tmp] update the ll2vec program

--- tmp_code/fe_program_llm2vec.py
+++ tmp_code/fe_program_llm2vec.py
@@ -6,6 +6,11 @@ import rampwf as rw
 from ramphy import ramp_setup as rs
 
 os.environ["PANGU_PATH"] = "/home/gpaolo/pangu-agent"
+os.environ["HF_HUB_OFFLINE"] = "0"
+os.environ["TRANSFORMERS_OFFLINE"] = "0"
+os.environ["CURL_CA_BUNDLE"] = ""
+
+# os.system("proxy")
 
 KITS_ROOT = Path("/home/gpaolo/ramp-kits")
 KIT_NAME = "kaggle_synthanic_v1_1_n1"
@@ -15,26 +20,22 @@ model = "lgbm"
 
 NEW_SUBMISSION_NAME = "lgbm_llm2vec"
 
+if __name__ == "__main__":
+    rs.tabular_classification_columnwise_last_submit(
+        submission="llm2vec",
+        classifier=model,
+        data_preprocessors=["llm_text2vec"],
+        text_col_encode=False,
+        ramp_kit_dir=KIT_PATH,
+    )
 
-rs.tabular_classification_columnwise_last_submit(
-    submission="llm2vec",
-    classifier=model,
-    data_preprocessors=["llm_text2vec"],
-    text_col_encode=False,
-    ramp_kit_dir=KIT_PATH,
-)
+    rh.submit_hybrid(
+        ramp_kit_dir=str(KIT_PATH),
+        new_submission=NEW_SUBMISSION_NAME,
+        parent_submissions={
+            "data_preprocessors": "llm2vec",
+            ("feature_extractor", "classifier"): "lgbm_hyperopt_best",
+        },
+    )
 
-# rh.submit_hybrid(
-#     ramp_kit_dir=str(KIT_PATH),
-#     new_submission=NEW_SUBMISSION_NAME,
-#     parent_submissions={
-#         "data_preprocessors": {
-#             "lgbm_hyperopt_best": "data_preprocessor_0_drop_id",
-#             "llm_rejector": "data_preprocessor_0_llm_drop_column",
-#         },
-#         "fe": {"lgbm_hyperopt_best": "feature_extractor"},
-#         "model": {"lgbm_hyperopt_best": "regressor"},
-#     },
-# )
-
-# rh.actions.train(ramp_kit_dir=str(KIT_PATH), submission=NEW_SUBMISSION_NAME, fold_idxs=range(900, 931))
+    rh.actions.train(ramp_kit_dir=str(KIT_PATH), submission=NEW_SUBMISSION_NAME, fold_idxs=range(900, 931))
