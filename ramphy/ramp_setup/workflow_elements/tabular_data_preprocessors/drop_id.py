ed211ca31b4eea9ffd74d5670e84f2ff724aad9c
Albert Thomas <albertthomas88@gmail.com>
2024-07-11 22:27:42 +0800

    do not drop id_col if it is the date col

--- ramphy/ramp_setup/workflow_elements/tabular_data_preprocessors/drop_id.py
+++ ramphy/ramp_setup/workflow_elements/tabular_data_preprocessors/drop_id.py
@@ -3,14 +3,20 @@ import pandas as pd
 from typing import Tuple
 import ramphy.ramp_setup as rs
 
+
 class DataPreprocessor(rs.BaseDataPreprocessor):
     """Drop ID column"""
 
     def preprocess(
-        self, X_train: pd.DataFrame, y_train: np.ndarray, X_test: pd.DataFrame, metadata: dict
+        self,
+        X_train: pd.DataFrame,
+        y_train: np.ndarray,
+        X_test: pd.DataFrame,
+        metadata: dict,
     ) -> Tuple[pd.DataFrame, np.ndarray, pd.DataFrame, dict]:
-        X_train = X_train.drop(columns=[metadata["id_col"]])
-        X_test = X_test.drop(columns=[metadata["id_col"]])
+        # the date column is sometimes the id column as well, in which case we should
+        # not remove it as it contains information.
+        if metadata["data_description"]["feature_types"][metadata["id_col"]] != "date":
+            X_train = X_train.drop(columns=[metadata["id_col"]])
+            X_test = X_test.drop(columns=[metadata["id_col"]])
         return X_train, y_train, X_test, metadata
-
-        
