49929eb331b983dce22b68644bfb89031a6e6556
Albert Thomas <albertthomas88@gmail.com>
2024-10-08 11:50:46 +0200

    mv kaggle interface to ramp-kaggle

--- setup.py
+++ setup.py
@@ -59,11 +59,8 @@ setup(
         'console_scripts': [
             'ramp-hyperopt = ramphy.cli.hyperopt:start',
             'ramp-setup = ramphy.cli.setup:start',
-            'ramp-download-leaderboards = ramphy.cli.download_leaderboards:start',
             'ramp-hyperopt-race = ramphy.cli.hyperopt_race:start',
-            'ramp-update-results = ramphy.cli.update_results:start',
             'ramp-register-experiment = ramphy.cli.register_experiment:start',
-            'ramp-submit-to-kaggle = ramphy.cli.submit_to_kaggle:start',
             'ramp-blend-at-round = ramphy.cli.blend_at_round:start',
         ]
     }
