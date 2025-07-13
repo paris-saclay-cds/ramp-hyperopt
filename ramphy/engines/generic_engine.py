dd3f1adf0f89e226d2dd9545f7f40aab8473dd25
Balazs Kegl <balazs.kegl@gmail.com>
2025-07-13 19:14:22 +0800

    fixing base engine class

--- ramphy/engines/generic_engine.py
+++ ramphy/engines/generic_engine.py
@@ -2,25 +2,13 @@ from abc import ABC, abstractmethod
 
 
 class GenericEngine(ABC):
-    def __init__(self):
+    def __init__(self, hyperparameters):
         pass
 
     @abstractmethod
-    def next_hyperparameter_indices(self, df_scores, n_folds):
+    def next_hyperparameter_indices(self, df_scores, problem):
         pass
 
     @abstractmethod
-    def pass_feedback(self, fold_i, n_folds, df_scores, score_name):
+    def pass_feedback(self, df_scores, problem):
         pass
-
-    def finish_incomplete_cvs(self, incomplete_folds, n_folds):
-        incomplete_folds = incomplete_folds.reset_index()
-        next_values = incomplete_folds.iloc[0][
-            [h.name for h in self.hyperparameters]
-        ].values
-        next_value_indices = [
-            h.get_index(v) for h, v in zip(self.hyperparameters, next_values)
-        ]
-        # for some reason iloc converts int to float
-        fold_i = int(incomplete_folds.iloc[0]["fold_i"]) % n_folds
-        return fold_i, next_value_indices
